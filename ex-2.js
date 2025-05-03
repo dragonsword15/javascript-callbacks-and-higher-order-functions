1b44172d54a30ca3daab7298f4e208c24d8d46a9
pongpetr <suerdragon8995@gmail.com>
2025-05-04 03:00:58 +0700

    bp8-pang

--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,13 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let m = 0;
+  for(let i = 0; i < array.length; i++) {
+    if(operation(array[i])) {
+      m++;
+    }
+  }
+  return m >= 5
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -9,7 +16,11 @@ const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
+function scoreUnder70(score) {
+  return score > 70;
+}
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, scoreUnder70)
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, scoreUnder70)
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, scoreUnder70)
+console.log(scoreRoom2Result)
